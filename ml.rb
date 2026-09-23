403a41c2763d5b77815565875f783ea138735793
Aliancn <Aliancn@users.noreply.github.com>
2026-09-23 04:15:28 +0000

    Commit from GitHub Actions (Update formulae)

--- ml.rb
+++ ml.rb
@@ -1,27 +1,27 @@
 class Ml < Formula
   desc 'ml is a Markdown live viewer that opens .md files in a browser'
-  version '0.1.0'
+  version '0.1.2'
   homepage 'https://github.com/Aliancn/mdlive'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/Aliancn/mdlive/releases/download/v0.1.0/mdlive_v0.1.0_darwin_arm64.zip'
-      sha256 '1f925c5ba977ea8f80e49fa21f52e87024397c1a4727231dc6b002da7a334fe4'
+      url 'https://github.com/Aliancn/mdlive/releases/download/v0.1.2/mdlive_v0.1.2_darwin_arm64.zip'
+      sha256 '41b9b620404ed25e5a0fd66f289e86a00a4f4e7cc17c411b5e45ce97bb412aad'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Aliancn/mdlive/releases/download/v0.1.0/mdlive_v0.1.0_darwin_amd64.zip'
-      sha256 '9dd406af36b2372dc071d1e6102bbe9feaa3ad0d86ed2014d1493486dacd494b'
+      url 'https://github.com/Aliancn/mdlive/releases/download/v0.1.2/mdlive_v0.1.2_darwin_amd64.zip'
+      sha256 'e001741d837156580ebe3fe0a832fa5b8619b0e104396274ca389e2c15840dc8'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/Aliancn/mdlive/releases/download/v0.1.0/mdlive_v0.1.0_linux_arm64.tar.gz'
-      sha256 'b578becb23dfc50c60c7260cf73b47545fb17f2623ef99352f4ec84a5f550d6b'
+      url 'https://github.com/Aliancn/mdlive/releases/download/v0.1.2/mdlive_v0.1.2_linux_arm64.tar.gz'
+      sha256 'e758a3386fc206ede10a128a82b0793ac6cca2f03942ce20703433c3929bfd13'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Aliancn/mdlive/releases/download/v0.1.0/mdlive_v0.1.0_linux_amd64.tar.gz'
-      sha256 'ede2fa756abc85a857ddfda4d7ea164515ebc5f38dcd813de073beba82ad4faf'
+      url 'https://github.com/Aliancn/mdlive/releases/download/v0.1.2/mdlive_v0.1.2_linux_amd64.tar.gz'
+      sha256 'fecaa4b1dd3b6e982c8b5be99de01d06e062e2702fa0d09ab64f55302f0e020b'
     end
   end
 
